6219ad3394e2723429f81b0d0bb3084f9eab8179
Sriharsha Gangam <sriharsha.gangam@turn.com>
2015-08-06 15:13:09 -0700

    Jedis client fixes
    Jira: TDB-166
    Signed-off-by: Sriharsha Gangam <sgangam@turn.com>

--- src/main/java/com/turn/splicer/cache/JedisClient.java
+++ src/main/java/com/turn/splicer/cache/JedisClient.java
@@ -3,8 +3,8 @@ package com.turn.splicer.cache;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author sgangam
@@ -12,37 +12,30 @@ import java.util.Set;
 public class JedisClient {
 
 
-	private JedisCluster jc;
+	//The JedisCluster is threadsafe
+	private static final HostAndPort[] hostAndPorts = {
+			new HostAndPort("127.0.0.1", 30001),
+			new HostAndPort("127.0.0.1", 30002),
+			new HostAndPort("127.0.0.1", 30003),
+			new HostAndPort("127.0.0.1", 30004),
+			new HostAndPort("127.0.0.1", 30005),
+			new HostAndPort("127.0.0.1", 30006)
+	};
+	private static final JedisCluster jc = new JedisCluster(
+			new HashSet<HostAndPort>(Arrays.asList(hostAndPorts)));
 
 	public static void main(String[] args) {
 		JedisClient jc = new JedisClient();
-		jc.init();
 
 		jc.put("a", "A1");
 		jc.put("b", "B1");
 		jc.put("c", "C1");
 
 		System.out.println(jc.get("a") + " " + jc.get("b") + " " + jc.get("c"));
-		jc.deinit();
 	}
 
 	private void put(String key, String value) {
 		this.jc.set(key, value);
-	}
-
-	private void init() {
-		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30001));
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30002));
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30003));
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30004));
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30005));
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30006));
-		this.jc = new JedisCluster(jedisClusterNodes);
-	}
-
-	private void deinit() {
-		//NO OP
 	}
 
 	private String get(String key) {
