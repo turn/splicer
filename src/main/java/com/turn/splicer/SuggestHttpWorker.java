8429d2d436d6e8e3f857e411782bb83ec5a61c20
Brian Peltz <brian.peltz@turn.com>
2015-10-20 19:25:20 -0700

    Implement the Suggest Api -for GET: send a GET request as is to a random TSDB node and return result -for POST: parse fields out and construct a GET request for random TSDB node shit needs some refactoring: there is a lot of duplicated logic from my SuggestHttpWorker copying from HttpWorker - I'll revisit this if we decide to implement more of the api's
    Signed-off-by: Brian Peltz <brian.peltz@turn.com>

---
+++ src/main/java/com/turn/splicer/SuggestHttpWorker.java
@@ -0,0 +1,101 @@
+package com.turn.splicer;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Takes a api/suggest query string and runs it against a random TSD node,
+ * returns the result of the query
+ *
+ * TODO: needs to be refactored with HttpWorker, there is a lot of duplicate logic
+ * TODO: error reporting differs from regular TSDB - no error message for invalid type
+ */
+public class SuggestHttpWorker implements Callable<String> {
+	private static final Logger LOG = LoggerFactory.getLogger(SuggestHttpWorker.class);
+
+	private final String suggestQuery;
+
+	private final String[] hosts;
+
+	private static final Random RANDOM_GENERATOR = new Random();
+
+	public SuggestHttpWorker(String queryString) {
+		this.suggestQuery = queryString;
+
+		Set<String> hosts = HttpWorker.TSDMap.keySet();
+		if (hosts.size() == 0) {
+			throw new NullPointerException("No Query Hosts. TSDMap.size = 0");
+		}
+
+		this.hosts = new String[hosts.size()];
+		int i=0;
+		for (String h: hosts) {
+			this.hosts[i] = h;
+			i++;
+		}
+	}
+
+	@Override
+	public String call() throws Exception {
+		LinkedBlockingQueue<String> TSDs;
+
+		//TODO: have it implement its own RegionChecker to get hbase locality looking for metric names
+		//lets have it just pick a random host
+		String hostname = getRandomHost();
+		TSDs = HttpWorker.TSDMap.get(hostname);
+
+		if (TSDs == null) {
+			LOG.error("We are not running TSDs on regionserver={}. Choosing a random host failed", hostname);
+			return "{'error': 'Choice of hostname=" + hostname + " failed.'}";
+		}
+
+		String server = TSDs.take();
+		String uri = "http://" + server + "/api/suggest?" + suggestQuery;
+
+		CloseableHttpClient postman = HttpClientBuilder.create().build();
+		try {
+			HttpGet getRequest = new HttpGet(uri);
+
+			LOG.info("Sending query=" + uri + " to TSD running on host=" + hostname);
+
+			HttpResponse response = postman.execute(getRequest);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatusLine().getStatusCode());
+			}
+
+			List<String> dl = IOUtils.readLines(response.getEntity().getContent());
+			String result = StringUtils.join(dl, "");
+			LOG.info("Result={}", result);
+
+			return result;
+		} finally {
+			IOUtils.closeQuietly(postman);
+
+			TSDs.put(server);
+			LOG.info("Returned {} into the available queue", server);
+		}
+	}
+
+	/**
+	 * Chooses a random number between 0 - (host.length-1) inclusive
+	 * returns that index in the hosts list
+	 * @return
+	 */
+	private String getRandomHost() {
+		return hosts[RANDOM_GENERATOR.nextInt(hosts.length)];
+	}
+}
---
+++ src/main/java/com/turn/splicer/SuggestServlet.java
@@ -0,0 +1,116 @@
+package com.turn.splicer;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Implements the suggest api for requests with api/suggest by passing the
+ * request on to a TSDB node and returning the result
+ * for example: TSDB_HOST/api/suggest?type=metrics&q=tcollector"
+ * should give you the first 25 metrics that start with "tcollector"
+ *
+ * Created by bpeltz on 10/19/15.
+ */
+public class SuggestServlet extends HttpServlet {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SuggestServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		try {
+			SuggestHttpWorker worker = new SuggestHttpWorker(request.getQueryString());
+			String json = worker.call();
+
+			response.setContentType("application/json");
+			response.getWriter().write(json);
+			response.getWriter().flush();
+		} catch (IOException e) {
+			LOG.error("IOException which processing GET request", e);
+		} catch (Exception e) {
+			LOG.error("Exception which processing GET request", e);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		//ok let's see if I can build the query string out of the request object
+		BufferedReader reader = request.getReader();
+		String line;
+		StringBuilder builder = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+		}
+
+		String jsonPostRequest = builder.toString();
+
+		JSONObject obj = null;
+
+		try {
+			obj = new JSONObject(jsonPostRequest);
+		} catch (JSONException e) {
+			LOG.error("Exception reading POST data " + jsonPostRequest);
+			response.getWriter().write("Exception reading POST data " + jsonPostRequest);
+			return;
+		}
+
+		String type = null;
+
+		try {
+			type = obj.getString("type");
+		} catch (JSONException e) {
+			LOG.error("No type provided for suggest request " + jsonPostRequest);
+			response.getWriter().write("No type provided for suggest request " + jsonPostRequest);
+			return;
+		}
+
+		StringBuilder getRequestString = new StringBuilder();
+		getRequestString.append("type=");
+		getRequestString.append(type);
+
+		String q = null;
+		Integer max = null;
+		//q is optional
+		try {
+			q = obj.getString("q");
+		} catch (JSONException e) {}
+
+		//max is optional
+		try {
+			max = obj.getInt("max");
+		} catch (JSONException e) {}
+
+		if (q != null) {
+			getRequestString.append("&q=");
+			getRequestString.append(q);
+		}
+
+		if (max != null) {
+			getRequestString.append("&max=");
+			getRequestString.append(max);
+		}
+		String requestString = getRequestString.toString();
+
+		SuggestHttpWorker worker = new SuggestHttpWorker(requestString);
+		String json = null;
+		try {
+			json = worker.call();
+		} catch (Exception e) {
+			LOG.error("Exception processing POST reqeust", e);
+			response.getWriter().write("Exception processing POST request" + e);
+		}
+
+		response.setContentType("application/json");
+		response.getWriter().write(json);
+		response.getWriter().flush();
+	}
+}
