b90c1129aab07906246d0d6d4cd5344aba8a74bf
Arjun Satish <arjun.satish@turn.com>
2016-01-05 20:05:26 -0800

    SimpleDateFormat is not a thread safe class
    SDF should not be shared between threads.
    
    Signed-off-by: Arjun Satish <arjun.satish@turn.com>

--- src/main/java/com/turn/splicer/Const.java
+++ src/main/java/com/turn/splicer/Const.java
@@ -5,9 +5,8 @@ import java.util.Date;
 
 public class Const {
 
-	public static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
-
 	public static String tsFormat(long val) {
+		SimpleDateFormat SDF = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
 		return SDF.format(new Date(val));
 	}
 
