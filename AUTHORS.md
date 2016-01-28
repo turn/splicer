55716958b27258ca4d1fa8fd4851a50efd6041fa
Jonathan Creasy <jonathan.creasy@turn.com>
2016-01-28 15:32:32 -0600

    Starting Process to apply Apache v2 License

---
+++ AUTHORS.md
@@ -0,0 +1,9 @@
+h1. Corporate Authors
+
+- Turn Inc
+ - Arjun Satish <arjun.satish@gmail.com>
+ - Brian Peltz <brian.peltz@turn.com>
+ - Jonathan Creasy <jonathan.creasy@turn.com>
+
+h1. Individual Authors
+- __none yet__ *your name here*
--- src/main/java/com/turn/splicer/tsdbutils/expression/AggregationIterator.java
+++ src/main/java/com/turn/splicer/tsdbutils/expression/AggregationIterator.java
@@ -10,6 +10,7 @@
 // General Public License for more details.  You should have received a copy
 // of the GNU Lesser General Public License along with this program.  If not,
 // see <http://www.gnu.org/licenses/>.
+
 package com.turn.splicer.tsdbutils.expression;
 
 import com.turn.splicer.Const;
