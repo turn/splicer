90ddc019bc4d5467a83972aae8a4f596e007867c
Jonathan Creasy <jonathan.creasy@turn.com>
2016-02-17 15:39:38 -0600

    net.opentsdb.core.DataPoint are now based on lastest put (eaf0fdc9e36666937241e153df06ea61f121ec18)

--- src/main/java/net/opentsdb/core/DataPoint.java
+++ src/main/java/net/opentsdb/core/DataPoint.java
@@ -14,21 +14,19 @@ package net.opentsdb.core;
 
 /**
  * Represents a single data point.
- * <p/>
+ * <p>
  * Implementations of this interface aren't expected to be synchronized.
  */
 public interface DataPoint {
 
   /**
    * Returns the timestamp (in milliseconds) associated with this data point.
-   *
    * @return A strictly positive, 32 bit integer.
    */
   long timestamp();
 
   /**
    * Tells whether or not the this data point is a value of integer type.
-   *
    * @return {@code true} if the {@code i}th value is of integer type,
    * {@code false} if it's of doubleing point type.
    */
@@ -36,14 +34,12 @@ public interface DataPoint {
 
   /**
    * Returns the value of the this data point as a {@code long}.
-   *
    * @throws ClassCastException if the {@code isInteger() == false}.
    */
   long longValue();
 
   /**
    * Returns the value of the this data point as a {@code double}.
-   *
    * @throws ClassCastException if the {@code isInteger() == true}.
    */
   double doubleValue();
@@ -51,7 +47,6 @@ public interface DataPoint {
   /**
    * Returns the value of the this data point as a {@code double}, even if
    * it's a {@code long}.
-   *
    * @return When {@code isInteger() == false}, this method returns the same
    * thing as {@link #doubleValue}.  Otherwise, it returns the same thing as
    * {@link #longValue}'s return value casted to a {@code double}.
