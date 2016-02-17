da7de6282df39e67537346132a7c0a18131e2628
Jonathan Creasy <jonathan.creasy@turn.com>
2016-02-17 15:39:07 -0600

    net.opentsdb.core.SeekableView are now based on lastest put (eaf0fdc9e36666937241e153df06ea61f121ec18)

--- src/main/java/net/opentsdb/core/SeekableView.java
+++ src/main/java/net/opentsdb/core/SeekableView.java
@@ -12,24 +12,22 @@
 // see <http://www.gnu.org/licenses/>.
 package net.opentsdb.core;
 
-import net.opentsdb.core.DataPoint;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
  * Provides a <em>zero-copy view</em> to iterate through data points.
- * <p/>
+ * <p>
  * The iterator returned by classes that implement this interface must return
  * each {@link DataPoint} in {@code O(1)} and does not support {@link #remove}.
- * <p/>
+ * <p>
  * Because no data is copied during iteration and no new object gets created,
  * <b>the {@link DataPoint} returned must not be stored</b> and gets
  * invalidated as soon as {@link #next} is called on the iterator (actually it
  * doesn't get invalidated but rather its contents changes).  If you want to
  * store individual data points, you need to copy the timestamp and value out
  * of each {@link DataPoint} into your own data structures.
- * <p/>
+ * <p>
  * In the vast majority of cases, the iterator will be used to go once through
  * all the data points, which is why it's not a problem if the iterator acts
  * just as a transient "view".  Iterating will be very cheap since no memory
@@ -48,28 +46,25 @@ public interface SeekableView extends Iterator<DataPoint> {
    * No new object gets created, the referenced returned is always the same
    * and must not be stored since its internal data structure will change the
    * next time {@code next()} is called.
-   *
    * @throws NoSuchElementException if there were no more elements to iterate
-   *                                on (in which case {@link #hasNext} would have returned {@code false}.
+   * on (in which case {@link #hasNext} would have returned {@code false}.
    */
   DataPoint next();
 
   /**
    * Unsupported operation.
-   *
    * @throws UnsupportedOperationException always.
    */
   void remove();
 
   /**
    * Advances the iterator to the given point in time.
-   * <p/>
+   * <p>
    * This allows the iterator to skip all the data points that are strictly
    * before the given timestamp.
-   *
    * @param timestamp A strictly positive 32 bit UNIX timestamp (in seconds).
    * @throws IllegalArgumentException if the timestamp is zero, or negative,
-   *                                  or doesn't fit on 32 bits (think "unsigned int" -- yay Java!).
+   * or doesn't fit on 32 bits (think "unsigned int" -- yay Java!).
    */
   void seek(long timestamp);
 
